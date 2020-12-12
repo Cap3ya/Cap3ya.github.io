f47dc6472e3a41579887a89ecba32d1706738c4e
capeya92 <capeya92@gmail.com>
2020-12-12 10:52:28 +0100

    modified:   DirectoryTraversal.sh

--- DirectoryTraversal/DirectoryTraversal.sh
+++ DirectoryTraversal/DirectoryTraversal.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 dns="${1}"
 paths=paths.txt
-<<<<<<< HEAD
 file="${2}"
-=======
-files=$2
->>>>>>> 167bd0da3874caa0ab96ee098a7d1c6bdd11444c
 translations=translations.txt
 
 for translation in $(cat $translations)
@@ -13,12 +9,7 @@ for translation in $(cat $translations)
 		for path in $(cat $paths)
 		do
 			data=$(echo $dns$path$file | sed $translation)
-<<<<<<< HEAD
 			curl --include $data |& grep -E '\<200\>' >/dev/null && curl --include $dns$path$file | less
-=======
-			curl --silent --head $data | grep -E '\<200\>' > /dev/null \
-			       && curl --inclue $dns$path$file 2> /dev/null
->>>>>>> 167bd0da3874caa0ab96ee098a7d1c6bdd11444c
 		done
 	done
 exit 0
