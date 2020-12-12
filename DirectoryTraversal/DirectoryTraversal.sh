355615fcebc4e62c1969610f2e7bbb2b3424d514
capeya92 <capeya92@gmail.com>
2020-12-12 12:18:05 +0100

    modified:   DirectoryTraversal/DirectoryTraversal.sh 	modified:   Wordlists/SubDomains[a-z0-9].txt 	modified:   Wordlists/SubDomains[a-z0-9_-]&gt1.txt 	modified:   Wordlists/SubDomains[a-z0-9_-].txt

--- DirectoryTraversal/DirectoryTraversal.sh
+++ DirectoryTraversal/DirectoryTraversal.sh
@@ -9,7 +9,8 @@ for translation in $(cat $translations)
 		for path in $(cat $paths)
 		do
 			data=$(echo $dns$path$file | sed $translation)
-			curl --include $data |& grep -E '\<200\>' >/dev/null && curl --include $dns$path$file | less
+			echo $data
+			curl --include $data |& grep -E '\<200\>' > /dev/null && curl --include $dns$path$file | less
 		done
 	done
 exit 0
