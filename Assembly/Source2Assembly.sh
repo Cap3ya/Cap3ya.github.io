d39b16a84cee47ec58d7a8827df2862b3a3affbb
capeya92 <capeya92@gmail.com>
2021-02-05 19:26:44 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .Notes.txt
    	modified:   Assembly/Source2Assembly.sh
    	new file:   Assembly/exit/exit
    	new file:   Assembly/exit/exit.s

--- Assembly/Source2Assembly.sh
+++ Assembly/Source2Assembly.sh
@@ -20,8 +20,8 @@
 	as $1.s -o $1.o 	|| exit 5 \
 	&& ld $1.o -o $1 	|| exit 4 \
 	&& rm $1.o 		|| exit 3 \
-	&& mkdir $1d 		|| exit 2 \
-	&& mv $1.s $1 $1d 	|| exit 1
+	&& mkdir $1 		|| exit 2 \
+	&& mv $1.s $1 $1/ 	|| exit 1
 
 # Exit:
 	exit 0
---
+++ Assembly/exit/exit.s
@@ -0,0 +1,31 @@
+#PURPOSE	Simple program that exits and returns a
+#		status code back to the Linux Kernel 
+
+#INPUT		None
+
+#OUTPUT		Returns a status code. 
+#		This can be viewed by typing 
+#		echo $?
+#		after running the program
+
+#VARIABLES	%eax holds the system call number
+#		%ebx holds the return status 
+
+.section .data
+
+.section .text
+
+.globl _start
+
+_start:
+
+movl $1, %eax	# This is the linux kernel command 
+		# number (system call) for exiting 
+		# a program. 
+
+movl $0, %ebx	# This is the status number we will 
+		# return to the operating system.
+
+int $0x80	# This wakes up the kernel to run 
+		# the exit command. 
+
